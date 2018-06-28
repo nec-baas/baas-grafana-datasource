b61b831e0ce53da0eefb602877ef9f5699adb2fc
Takuya Murakami <murakami@da.jp.nec.com>
2018-06-28 15:46:59 +0900

    update modules

--- src/module.ts
+++ src/module.ts
@@ -2,12 +2,16 @@ import {BaasDatasource} from "./datasource";
 import {BaasDatasourceQueryCtrl} from "./query_ctrl";
 
 class BaasConfigCtrl {
-    static templateUrl: string
+    static templateUrl = 'partials/config.html';
 }
-BaasConfigCtrl.templateUrl = 'partials/config.html';
+
+class BaasQueryOptionsCtrl {
+    static templateUrl = 'partials/query.options.html';
+}
 
 export {
     BaasDatasource as DataSource,
     BaasDatasourceQueryCtrl as QueryCtrl,
-    BaasConfigCtrl as ConfigCtrl
+    BaasConfigCtrl as ConfigCtrl,
+    BaasQueryOptionsCtrl as QueryOptionsCtrl
 };
--- src/query_ctrl.ts
+++ src/query_ctrl.ts
@@ -11,5 +11,21 @@ declare class QueryCtrl {
 }
 
 export class BaasDatasourceQueryCtrl extends QueryCtrl {
+    static templateUrl = 'partials/query.editor.html';
 
+    constructor($scope: any, $injector: any) {
+        super($scope, $injector);
+    }
+
+    getOptions(query: any) {
+        return this.datasource.metricFindQuery('');
+    }
+
+    toggleEditorMode() {
+        this.target.rawQuery = !this.target.rawQuery;
+    }
+
+    onChangeInternal() {
+        this.panelCtrl.refresh();
+    }
 }
