4b87dbc2518d4126b65f711c75b81bb42af30bc4
Takuya Murakami <murakami@da.jp.nec.com>
2018-06-28 14:52:39 +0900

    add query impl.

--- src/datasource.ts
+++ src/datasource.ts
@@ -1,27 +1,31 @@
 //import {Promise} from 'es6-promise';
 declare var Promise: any;
 
+import * as _ from 'lodash';
+
 export class BaasDatasource {
     name: string;
     baseUri: string;
+    tenantId: string;
     headers: any;
 
     backendSrv: any;
+    templateSrv: any;
 
     /**
      * コンストラクタ
      * @param instanceSettings 設定値。config.html で設定したもの。
      * @param backendSrv Grafana の BackendSrv。
      * @param $q Angular非同期サービス($q service)
-     * @param templateSrv Grafana の TemplateSrv。本 plugin では使用しない。
+     * @param templateSrv Grafana の TemplateSrv。
      */
     constructor(instanceSettings: any, backendSrv: any, $q: any, templateSrv: any) {
         this.name = instanceSettings.name;
 
         this.baseUri = instanceSettings.baseUri;
+        this.tenantId = instanceSettings.tenantId;
         this.headers = {
             "Content-Type": "application/json",
-            "X-Tenant-Id": instanceSettings.tenantId,
             "X-Application-Id": instanceSettings.appId,
             "X-Application-Key": instanceSettings.appKey
         };
@@ -37,8 +41,78 @@ export class BaasDatasource {
      * @param options
      */
     query(options: any) {
+        const query = this.buildQueryParameters(options);
+        query.targets = query.targets.filter(t => !t.hide);
 
+        if (query.targets.length <= 0) {
+            return Promise.resolve({data: []}); // no targets
+        }
+
+        //const allResults = [];
+        //const promises = [];
+
+        let bucketName: string = null;
+        const fieldNames: [string] = [] as [string];
+
+        for (let i = 0; i < query.targets.lengh; i++) {
+            // metric target: バケット名, field名
+            const target = query.targets[i].target;
+            const a = target.split(":", 2);
+            if (i == 0) {
+                bucketName = a[0];
+            } if (i > 0 && bucketName != a[0]) {
+                return Promise.reject(new Error("bucket names mismatch."))
+            }
+            const fieldName = a[1];
+            fieldNames.push(fieldName);
+        }
+
+        // URI for long query
+        const uri = this.baseUri + "/1/" + this.tenantId + "/objects/" + bucketName + "/_query";
+
+        const where = {
+            "$and": [
+                {"createdAt": {"$gte": options.range.from}},
+                {"createdAt": {"$lte": options.range.to}},
+            ]
+        };
+
+        const promise = this.doRequest({
+            "url": uri,
+            "data": {
+                "where": where,
+                "order": "createdAt",
+                "limit": options.maxDataPoints
+            },
+            "method": "POST"
+        }).then(response => {
+            const status = response.status;
+            const data = response.data;
+
+            return this.convertResponse(query.targets, fieldNames, data);
+        });
     }
+
+    private convertResponse(targets: [string], fieldNames: [string], data: any): any {
+        const results = [];
+
+        for (let i = 0; i < targets.length; i++) {
+            // datapoints に変換
+            const datapoints = _.map(data.results, e => {
+                const value = e[fieldNames[i]]; // TBD
+                const ts = new Date(e["createdAt"]);
+                return [value, ts.getTime()];
+            });
+
+            results.push({
+                "target": targets[i],
+                "datapoints": datapoints
+            });
+        }
+
+        return results;
+    }
+
 
     /**
      * Datasource接続テスト
@@ -63,8 +137,25 @@ export class BaasDatasource {
         return Promise.resolve([]);
     }
 
-    private doRequest(options: any) {
+    private doRequest(options: any): any {
         options.headers = this.headers;
         return this.backendSrv.datasourceRequest(options);
+    }
+
+    private buildQueryParameters(options: any): any {
+        options.targets = _.filter(options.targets, target => {
+            return target.target !== 'select metric';
+        });
+
+        const targets = _.map(options.targets, target => {
+            return {
+                target: this.templateSrv.replace(target.target, options.scopedVars, 'regex'),
+                refId: target.refId,
+                hide: target.hide,
+                type: target.type || 'timeserie'
+            }
+        });
+        options.targets = targets;
+        return options;
     }
 }
